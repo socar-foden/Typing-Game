10cb279b6361c15f084ee85147e476f23c8d0b29
brandon <zxczoxc125@naver.com>
2021-05-15 11:02:37 +0900

    # 폴리필 추가

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  entry: "./src/index.js",
+  entry: ["@babel/polyfill", "./src/index.js"],
   output: {
     path: __dirname + "/public",
   },
